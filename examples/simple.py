3297e17491f483a1cce6fdcc67b07527fac0e4da
Nickolay Chistov <nikola-chistov@mail.ru>
2023-06-05 11:58:59 +0300

    init

---
+++ examples/simple.py
@@ -0,0 +1,3 @@
+from py_dyt import translate
+
+print(translate('Hello, $name$!', {'name': 'Nick'}))
---
+++ src/py_dyt/translate.py
@@ -0,0 +1,30 @@
+from typing import Any
+from enum import Enum, auto
+
+
+class State(Enum):
+    WAITING = auto()
+    IN_VARIABLE = auto()
+
+
+def translate(string: str, variables: dict[str, Any]) -> str:
+    result = ''
+
+    variable = ''
+    state = State.WAITING
+
+    for letter in string:
+        if state == State.WAITING and letter == '$':
+            state = State.IN_VARIABLE
+        elif state == State.IN_VARIABLE:
+            if letter == '$':
+                state = State.WAITING
+
+                result += variables[variable]
+                variable = ''
+            else:
+                variable += letter
+        else:
+            result += letter
+
+    return result
