90f3791f08f77d8305d9c306b95c05c0e8f68d5d
Nickolay Chistov <nikola-chistov@mail.ru>
2023-06-06 10:00:50 +0300

    change quotes

--- examples/number.py
+++ examples/number.py
@@ -1,3 +1,3 @@
 from py_dyt import translate
 
-print(translate("Hello, $name$! It's $time$ o'clock", {"name": "Nick", "time": 5}))
+print(translate("Hello, $name$! It's $time$ o'clock", {'name': 'Nick', 'time': 5}))
